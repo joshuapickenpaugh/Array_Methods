a64ad8be2248699a2320a07c0c07268557de5772
Joshua Pickenpaugh <joshuapickenpaugh@hotmail.com>
2017-11-22 09:42:18 -0600

    Last commit on Wednesday.

--- Array_Methods/Program.cs
+++ Array_Methods/Program.cs
@@ -14,7 +14,62 @@ namespace Array_Methods
     {
         static void Main(string[] args)
         {
+            //Variables:
+            List<double> lstDouble = new List<double>();
+            double dblUserNum = 0;
+            Boolean bolLoopControl = true;
+            string strLoopControl;
 
+            //Greeting:
+            Console.WriteLine("Enter a number or 'X' to quit number entry: ");
+
+            //User input:
+            do
+            {
+                //User input:
+                Console.Write("--->: ");
+
+                strLoopControl = Console.ReadLine();
+                if (strLoopControl == "X" || strLoopControl =="x")
+                {
+                    bolLoopControl = false;
+                }
+                else
+                {
+                    dblUserNum = Convert.ToDouble(strLoopControl);
+                    lstDouble.Add(dblUserNum);
+                }
+            }
+            while (bolLoopControl == true);
+
+            //Methods:
+            DisplayArray(lstDouble);
+            DisplayInReverse(lstDouble);
+
+            //Exiting program:
+            Console.WriteLine("Press any key to EXIT PROGRAM.");
+            Console.ReadKey();
+        }
+
+        public static void DisplayArray(List<double> lstDbl)
+        {
+            Console.WriteLine("_______");
+            Console.WriteLine("Array: ");
+            foreach (double number in lstDbl)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void DisplayInReverse(List<double> lstDbl)
+        {
+            Console.WriteLine("_______");
+            Console.WriteLine("Array in Reverse Order: ");
+            lstDbl.Reverse();
+            foreach (double number in lstDbl)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
